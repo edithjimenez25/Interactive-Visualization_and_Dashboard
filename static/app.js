fbfdb6717cc7647d7ff3388e32822690cc9dda15
Edith Jimenez <57966386+edithjimenez25@users.noreply.github.com>
2020-03-31 01:16:46 -0500

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -6,19 +6,19 @@
 // Bring the overall data from samples.json that we are going to use
 // on the graphs and ID selection.
 
-/**********************************************************************************
-* Initialize                                                                      *
-***********************************************************************************/
+/*************************************************************************************************
+* Initialize                                                                                     *
+**************************************************************************************************/
 // samples.json have three panel of data: names, metadata and sample
 // the panel "names" will be used in the dropdown menu to select the subject 'id' for the chart
 
-/************************************************************************************
- * Build Table to show demographic data for selected 'Subject id'                   *
- ************************************************************************************/ 
+/*************************************************************************************************
+ * Build Table to show demographic data for selected 'Subject id'                                *
+ *************************************************************************************************/ 
 // Bring the data from the JSON file to build data for the table in 'Demographic Info'
 
 function buildMetadata(selectSubject) {
-  d3.json("/samples.json").then(function(allData) {
+  d3.json("/data/samples.json").then(function(allData) {
 
     // bring data
     // let data = allData;
@@ -38,44 +38,15 @@ function buildMetadata(selectSubject) {
 
     // Use `Object.entries` to add each key and values to the panel
     // R5. Display each key-value pair from the metadata JSON object somewhere on the page.      
-    object.entries(filteredMetadata).forEach(function([key,value]){        
+    Object.entries(filteredMetadata).forEach(function([key,value]) {        
       // Use d3 to append new tags for each key-value in the metadata.
-      var row = sample_Metadata.append("p");
-      row.text(`${key}:${value}`);
+      sample_Metadata.append("h6").text(`${key.toUpperCase()}:${value}`);
     });
 
     // print the filtered data to the console
     console.log(filteredMetadata);
 
   });
-
-  // load the data with the function buildMetadata
-  buildMetadata()
-
-  // Establish the reference between the element in the html and load data on the demografoc table. 
-  var inputID = d3.select("#subjectid");
-  var inputAge = d3.select("#age");
-  var inputGender = d3.select("#gender");
-  var inputEthnicity = d3.select("#ethnicity");
-  var inputBbtype = d3.select("#bbtype");
-  var inputWashFreq = d3.select('#wfreq');
-  var inputLocation = d3.select('#location')
- 
-  // create the connection with the selection button to filter the data by "SubjectID"
-  function filterData() {
-    
-    // set up the website and prevent it to refresh
-    d3.event.preventDefault();
-
-    // extract the input selected for all the row "subjectid", "age", "gender", "ethnicity", "bbtype", "wfreq", "location"
-    var idValue = inputID.property("value");
-    var ageValue = inputAge.property("value");
-    var genderValue = inputGender.property("value");
-    var ethnicityValue = inputEthnicity.property("value");
-    var bbtypeValue = inputBbtype.property("value");
-    var washFreqValue = inputWashFreq.property("value");
-    var locationValue = inputLocation.property("value");
-  }
 
 }
 
@@ -91,32 +62,35 @@ function buildCharts(selectSubject) {
 
   // Use d3.Json to fetch the sample panel values in the samples.
   
-  d3.json("/samples.json").then(function(allData){
+  d3.json("/data/samples.json").then(function(allData){
+
     // Assign var to the sample in sample
-    var sampleSamples = allData.samples;
+    var samples = allData.samples;
+
+    // Filter the data for subject id
+    var filteredSample = samples.filter(x => x.id == selectSubject);
+    var targetSample = filteredSample[0];
 
     // Build the bubble chart using the sample data
     // Define values for X axis and use 'otu_ids'
-    var x_values = allData.otu.ids;
-    var y_values = allData.sample_values;
-    var m_size = allData.sample_values;
-    var m_colors = allData.otu_ids; 
-    var t_values = allData.otu_labels;
-
     // Use slice to display the chart the top 10 OTUs
-    x_sliced = x_values.slice(0,10);
-    y_sliced = y_values.slice(0,10);
-    t_sliced = t_values.slice(0,10);
-    ms_sliced = m_size.slice(0,10);
-    mc_sliced = m_colors.sliced(0,10);
-      
+    var otu_ids_values = targetSample.otu_ids.slice(0,10);
+    var sample_values = targetSample.sample_values.slice(0,10);
+    var m_size = targetSample.sample_values.slice(0,10);
+    var m_colors = targetSample.otu_ids.slice(0,10); 
+    var otu_label_values = targetSample.otu_labels.slice(0,10);
+    
+    var y_label = targetSample.otu_ids.slice(0, 10).map(function(x){
+      return "OTU " + x;
+    }).reverse();
+
     // Create a trace object with the data
     var trace1 = {
       type: 'bar',
       orientation: "h",
-      y: x_sliced ,
-      x: y_sliced,
-      hovertext: t_sliced,
+      x: sample_values,
+      y: y_label,
+      hovertext: otu_label_values,
     };
   
     // Create a data array with the above trace
@@ -126,14 +100,16 @@ function buildCharts(selectSubject) {
     var layout = {
       yaxis: {autoarange: "reversed"},
       title: "Bacterias",
-      xaxis: { title: "OTU"},
-      yaxis: { title: "Samples"}
+      xaxis: { 
+        title: "OTU",
+      },
+      
     };
   
     // Render the plot 
     Plotly.newPlot("bar", data, layout); 
-  });    
-}
+    
+
 
 /************************************************************************************
  * Build Bubble Chart for selected 'Subject id'                                     *
@@ -145,25 +121,16 @@ function buildCharts(selectSubject) {
      // * Use `otu_ids` for the marker colors.
      // * Use `otu_labels` for the text values.
      
-
-// Display the sample metadata for an individual's demographic information.
-function buildBubbleChart() {
-  var subjectID = d3.select("#selDataset");
-  d3.json("/samples.json").then(function(allData){
-
-    // Use d3 to select the panel with id of `#sample-metadata`
-    var sampleSamples = allData.samples;
-
-
+    // Create a trace object with the data
     var trace2 = {
-      x: x_sliced,
-      y: y_sliced,
-      text: t_sliced,
+      y: sample_values,
+      x: otu_ids_values,
+      text: otu_label_values,
       mode: 'markers',
       marker: {
-        color: mc_sliced,
-        size: ms_sliced
-      } 
+        color: m_colors,
+        size: m_size,
+      },
     };
 
     var data = [trace2];
@@ -174,9 +141,8 @@ function buildBubbleChart() {
 
     Plotly.newPlot('bubble', data, layout);
 
-  });
-};
-
+  }); 
+}
 
 /************************************************************************************
  * Build function Initiate 
@@ -189,7 +155,7 @@ function init() {
 
   /* Use the list of sample names to populate the select options and
      define the var for each of the panel of data in samples */ 
-  d3.json("/samples.json").then((allData) => {
+  d3.json("/data/samples.json").then((allData) => {
     // let data = allData
     var sampleName = allData.names;
 
@@ -207,7 +173,7 @@ function init() {
     // Populate the demographic table 
     buildMetadata(selectSubject)
 
-    // Build the  charts: horizontal bar chart and Bubble chart 
+    // Build the  horizontal bar chart and Bubble chart 
     buildCharts(selectSubject);
 
     // Buil the Gauge 
@@ -238,10 +204,16 @@ init();
  * Generate the Gauge to measure the washing frequency of the Belly Button                                                   *
  ************************************************************************************/
 
+
+/************************************************************************************
+ * Bonus:
+ * Generate the Gauge to measure the washing frequency of the Belly Button                                                   *
+ ************************************************************************************/
+
 function buildGauge(wfreq) {
   // Enter the washing frequency between 0 and 180 (half of a circle 360 degree)
   var level = parseFloat(wfreq) * 20;
-
+    
   // Calculate using trigonometric the meter points for gauge
 
   // dregree of the circle i.e half of circle
@@ -280,8 +252,17 @@ function buildGauge(wfreq) {
       textposition: "inside",
       marker: {
         colors: [
-          "#FEFE69","#EEFFBA", "#D6FA8C", "#BEED53", "#A5D721",
-          "#82B300", "#5D8700", "#447741", "#316033", "#006266"
+          "#006266",
+          "#447741",
+          "#316033", 
+          "#5D8700",
+          "#82B300",
+          "#A5D721", 
+          "#BEED53",
+          "#D6FA8C",
+          "#EEFFBA",
+          "white",   
+         
         ]
       },
       labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
@@ -303,9 +284,9 @@ function buildGauge(wfreq) {
         }
       }
     ],
-    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
-    height: 500,
-    width: 500,
+    title: "<b>Belly Button Washing Frequency</b><br><br><br><br><br><br><br><br><br><br><br>Scrubs per Week",
+    height: 450,
+    width: 450,
     xaxis: {
       zeroline: false,
       showticklabels: false,
@@ -323,6 +304,3 @@ function buildGauge(wfreq) {
   var GAUGE = document.getElementById("gauge");
   Plotly.newPlot(GAUGE, data, layout);
 }
-
-
-
